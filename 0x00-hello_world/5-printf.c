ec816b6fc6ed75b07838e0d2aba0c53b7d3169f8
kidist <kidistfentaneh1990@gmail.com>
2022-12-23 00:03:10 -0800

    t5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the  outcome is
+ * main - prints "with proper grammar, but the outcome is
  * a piece of art,"followed by newline
  * Return: Always o.
  *
@@ -10,7 +10,7 @@
 int main(void)
 
 {
-	printf("with proper grammar, but the  outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
